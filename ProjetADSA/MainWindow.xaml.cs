2b096804e137dcbc768970a4a2146f4388c4b436
emylz <eminyalaz@gmail.com>
2018-12-31 17:32:39 +0100

    Final design

--- ProjetADSA/MainWindow.xaml.cs
+++ ProjetADSA/MainWindow.xaml.cs
@@ -24,59 +24,6 @@ namespace ProjetADSA
         public MainWindow()
         {
             InitializeComponent();
-
-            /*string a = "";
-
-            foreach (Team p in t.Total_team)
-            {
-                a +=p.Name + " " + p.Coefficient + "\n";
-            }
-
-            test.Content = a;
-
-             string a = "";
-
-             foreach(Pool p in t.Array)
-             {
-                 a += p.ToString() + "\n";
-             }
-
-             test.Content = a;
-
-
-             t.getHead();
-             a = "";
-
-             foreach (Team p in t.DirectE.Head)
-             {
-                 a += p.ToString() + "\n";
-             }
-             foreach (Team p in t.DirectE.NoHead)
-             {
-                 a += p.ToString() + "\n";
-             }
-
-             test.Content = a;
-             a = " ";
-
-             t.DirectE.draw();
-
-             t.DirectE.DirectElim.tourSelction();
-
-             a+= t.DirectE.DirectElim.Root.leftLeaf.leftLeaf.match.Team1 + "\n";
-
-             t.DirectE.DirectElim.tourSelction();
-
-             a += t.DirectE.DirectElim.Root.rightLeaf.match.Team1 + "\n";
-
-             t.DirectE.DirectElim.tourSelction();
-
-             a+= t.DirectE.DirectElim.Root.match.Team1 + "\n";
-
-             t.DirectE.DirectElim.tourSelction();
-
-             a+= t.DirectE.DirectElim.Root.match.Winner;
-             test.Content = a;*/
         }
 
         private void manualButton_Click(object sender, RoutedEventArgs e)
